fdff564cd7eeb3fba4d62ee5b48a8a3db943e774
yokn <yokn@protonmail.com>
2021-01-28 13:36:40 +0300

    add flights model and associate it with airports

---
+++ db/migrate/20210128095954_create_flights.rb
@@ -0,0 +1,13 @@
+class CreateFlights < ActiveRecord::Migration[6.1]
+  def change
+    create_table :flights do |t|
+      t.references :from_airport, null: false, foreign_key: { to_table: 'airports' }
+      t.references :to_airport, null: false, foreign_key: { to_table: 'airports' }
+
+      t.datetime :datetime, null: false
+      t.integer :duration, null: false
+
+      t.timestamps
+    end
+  end
+end
