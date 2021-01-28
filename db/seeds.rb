d4ea0a924e88ea41738fa4a16eef264a0aafb9e6
yokn <yokn@protonmail.com>
2021-01-28 12:54:34 +0300

    create Airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Airport.create!([
+                  { code: 'SFO' },
+                  { code: 'JFK' },
+                  { code: 'LAX' },
+                  { code: 'DFW' }
+                ])
