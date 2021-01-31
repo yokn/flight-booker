31d16f133aa978ca49c1d57366ce4bcd47c05abc
yokn <yokn@protonmail.com>
2021-01-31 15:03:28 +0300

    seed more flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,20 @@ Airport.create!([
 
 Flight.create!([
                  { from_airport: Airport.find_by_id(1), to_airport: Airport.find_by_id(2),
-                   date: '2021-02-14', time: ' 09:00:00', duration: 320, vacant_seats: 14 },
+                   date: '2021-02-14', time: '09:00:00', duration: 320, vacant_seats: 14 },
+                 { from_airport: Airport.find_by_id(1), to_airport: Airport.find_by_id(2),
+                   date: '2021-03-14', time: '10:00:00', duration: 320, vacant_seats: 9 },
+                 { from_airport: Airport.find_by_id(1), to_airport: Airport.find_by_id(2),
+                   date: '2021-04-14', time: '11:00:00', duration: 320, vacant_seats: 8 },
+                 { from_airport: Airport.find_by_id(1), to_airport: Airport.find_by_id(2),
+                   date: '2021-05-14', time: '12:00:00', duration: 320, vacant_seats: 7 },
+
                  { from_airport: Airport.find_by_id(3), to_airport: Airport.find_by_id(4),
-                   date: '2021-05-16', time: ' 08:00:00', duration: 200, vacant_seats: 3 }
+                   date: '2021-05-16', time: '08:00:00', duration: 200, vacant_seats: 2 },
+                 { from_airport: Airport.find_by_id(3), to_airport: Airport.find_by_id(4),
+                   date: '2021-06-16', time: '18:00:00', duration: 200, vacant_seats: 61 },
+                 { from_airport: Airport.find_by_id(3), to_airport: Airport.find_by_id(4),
+                   date: '2021-07-16', time: '12:00:00', duration: 200, vacant_seats: 33 },
+                 { from_airport: Airport.find_by_id(3), to_airport: Airport.find_by_id(4),
+                   date: '2021-08-16', time: '23:00:00', duration: 200, vacant_seats: 123 }
                ])
